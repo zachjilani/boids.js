07a7b4ff6c741670ec56915fec797154ba921951
unknown <zach.jilani1@gmail.com>
2023-09-07 00:50:26 -0700

    added src/test folders. added base variables and functions for boids

---
+++ src/boids.js
@@ -0,0 +1,32 @@
+class Boids {
+
+  constructor(){
+
+    //base variable setup
+    this.position;
+    this.velocity;
+    this.acceleration;
+    this.seeingRadius;
+    this.maximumForce;
+    this.speed;
+    this.separationDistance;
+
+    //beyond base variables. This is dependent on what the
+    //boid objects will actually be visually
+    this.size;
+    this.color;
+
+  }
+  //base functions
+  cohesion() {
+
+  }
+
+  separation() {
+
+  }
+
+  alignment() {
+
+  }
+}
